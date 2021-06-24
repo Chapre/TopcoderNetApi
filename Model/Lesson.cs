b889b3d3226d192bfbf3fbfc3a3fb494beb5a1ab
Chapre <john.ukundji@gmail.com>
2021-06-24 10:35:48 +0100

    nop

--- Model/Lesson.cs
+++ Model/Lesson.cs
@@ -7,5 +7,50 @@ namespace TopcoderNetApi.Model
 {
     public class Lesson
     {
+        //Id: string (GUID)
+        //Name: string (250 chars)
+        //videoUrl: string (355 chars)
+        //Order: number
+        //sectionId: Foreign key to section.id
+        
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the video URL.
+        /// </summary>
+        /// <value>
+        /// The video URL.
+        /// </value>
+        public string VideoUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order.
+        /// </summary>
+        /// <value>
+        /// The order.
+        /// </value>
+        public int Order { get; set; }
+
+        /// <summary>
+        /// Gets or sets the section.
+        /// </summary>
+        /// <value>
+        /// The section.
+        /// </value>
+        public Section Section { get; set; }
     }
 }
--- Model/Section.cs
+++ Model/Section.cs
@@ -7,5 +7,38 @@ namespace TopcoderNetApi.Model
 {
     public class Section
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order.
+        /// </summary>
+        /// <value>
+        /// The order.
+        /// </value>
+        public int Order { get; set; }
+
+        /// <summary>
+        /// Gets or sets the course.
+        /// </summary>
+        /// <value>
+        /// The course.
+        /// </value>
+        public Course Course { get; set; }
+
+
     }
 }
